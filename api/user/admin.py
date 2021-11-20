71bcf30cba97d2d8dc3ea4e276aa85dd23fce07a
Clement Seconde-Kynnersely <c.sec.kyn@gmail.com>
2021-11-20 10:44:09 +0000

    updating admin

--- api/user/admin.py
+++ api/user/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from .models import Exercise, UserExerciseLog
+from .models import Exercise, UserExerciseLog, User
 from django.contrib.auth import get_user_model
 
 # Register your models here.
 User = get_user_model()
 
+
 admin.site.register(Exercise)
+admin.site.register(User)
 admin.site.register(UserExerciseLog)
